8f0a6f2003d08b26b0b4388c1be39c413e2ba3a3
Anish <anish.tharagar@gmail.com>
2015-10-03 00:35:08 +0530

    testing the new mongo_custom_collect_2d_graph_data function

--- stats_functions.py
+++ stats_functions.py
@@ -6,20 +6,18 @@ import stats_data_parser
 import stats_plot
 
 
-def stat_functions_usage(mongo_host,mongo_db_name,mongo_collection_name = [],functions = [],query = []):
+def stat_functions_usage(mongo_host,mongo_db_name,x_field_name, y_field_name,mongo_collection_name = [], functions = [],query = []):
 
 
-        return_data = mongo_connect.mongo_collect_data(mongo_host,mongo_db_name,mongo_collection_name)
-        #return_data = mongo_connect.mongo_query_data(mongo_host,mongo_db_name,mongo_collection_name,query)
+        #return_data = mongo_connect.mongo_collect_data(mongo_host,mongo_db_name,mongo_collection_name)
+        #x_axis_data_points, y_axis_data_points = stats_data_parser.stats_parser("2d_graph", return_data)
 
-######
+        return_data = mongo_connect.mongo_custom_collect_2d_graph_data(mongo_host,mongo_db_name,mongo_collection_name,x_field_name,y_field_name)
+        for i in range(0,1000):
+            print return_data[i]
 
-        x_axis_data_points, y_axis_data_points = stats_data_parser.stats_parser("2d_graph", return_data)
-        #print x_axis_data_points
-        #print y_axis_data_points
-
-        stats_plot.stats_plot_2d_graph("x_label_test","y_label_test",x_axis_data_points,y_axis_data_points)
+        #stats_plot.stats_plot_2d_graph("x_label_test","y_label_test",x_axis_data_points,y_axis_data_points)
 
 
-stat_functions_usage('127.0.0.1','raw_data',["graph_congruent_incongruent"])
+stat_functions_usage('127.0.0.1','raw_data','maxpressurei','rain',["nyc_subset"])
 
